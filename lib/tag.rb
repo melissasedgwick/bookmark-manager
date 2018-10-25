36f61f3afede93f1470a356b4d4a4d7412cd905a
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-25 16:10:55 +0100

    Added where method to tag class

--- lib/tag.rb
+++ lib/tag.rb
@@ -14,4 +14,11 @@ class Tag
     Tag.new(id: result[0]['id'], content: result[0]['content'])
   end
 
+  def self.where(bookmark_id:)
+    result = DatabaseConnection.query("SELECT tags.id, content FROM bookmarks_tags INNER JOIN tags ON tags.id = bookmarks_tags.tags_id WHERE bookmarks_tags.bookmarks_id = '#{bookmark_id}';")
+    result.map do |tag|
+      Tag.new(id: tag['id'], content: tag['content'])
+    end
+  end
+
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,6 +1,6 @@
 require 'tag'
 require 'database_helpers'
-require 'bookmark'
+require 'bookmark_tag'
 
 describe Tag do
 
@@ -9,6 +9,24 @@ describe Tag do
       tag = Tag.create(content: 'test tag')
       expect(tag).to be_a Tag
       expect(tag.content). to eq 'test tag'
+    end
+  end
+
+  describe '#where' do
+    it 'shows tags linked to a bookmark' do
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      tag1 = Tag.create(content: 'A tag')
+      tag2 = Tag.create(content: 'A second tag')
+
+      BookmarkTag.create(bookmark_id: bookmark.id, tag_id: tag1.id)
+      BookmarkTag.create(bookmark_id: bookmark.id, tag_id: tag2.id)
+
+      tags = Tag.where(bookmark_id: bookmark.id)
+      tag = tags.first
+      expect(tags.length).to eq 2
+      expect(tag).to be_a Tag
+      expect(tag.id).to eq tag1.id
+      expect(tag.content).to eq tag1.content
     end
   end
 
