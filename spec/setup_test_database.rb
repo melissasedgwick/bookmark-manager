aa787d406d4805f33dcbdc39648d98c56c44c75c
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-25 16:05:27 +0100

    Updated to truncate new tables

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -6,6 +6,6 @@ def setup_test_database
 
   connection = PG.connect(dbname: 'bookmark_manager_test')
 
-  connection.exec("TRUNCATE bookmarks, comments;")
+  connection.exec("TRUNCATE bookmarks, comments, tags, bookmarks_tags;")
 
 end
